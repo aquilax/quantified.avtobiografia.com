1a3eabe118e24576f3528e6f2e70455e9e38c652
aquilax <aquilax@gmail.com>
2017-07-23 08:43:23 +0200

    Daily update

--- bin/update.sh
+++ bin/update.sh
@@ -4,7 +4,7 @@ DATA="/home/aquilax/ledger/"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-php ${DIR}/generate.php "1 june 2017" today ~/ledger/ ${DIR}/../content/
+php ${DIR}/generate.php "-1 week" today ~/ledger/ ${DIR}/../content/
 cd ${DIR}/..
 git add .
 git commit -am "Daily update"
