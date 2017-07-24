e988d65a48e26cd2668856bcca9e30a557e43609
aquilax <aquilax@gmail.com>
2017-07-24 05:11:02 +0200

    Daily update

--- bin/update.sh
+++ bin/update.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 php ${DIR}/generate.php "-1 week" today ~/ledger/ ${DIR}/../
 cd ${DIR}/..
 git add .
-git commit -am "Daily update"
+git commit --no-gpg -am "Daily update"
 git push
