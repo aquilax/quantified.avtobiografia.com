a33e905d37ccd7945eb434fe629fabf741a02016
aquilax <aquilax@gmail.com>
2017-09-07 05:00:03 +0200

    Daily update

--- bin/update.sh
+++ bin/update.sh
@@ -5,7 +5,7 @@ DATA="/home/aquilax/ledger/"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 php ${DIR}/generate.php "- 1 week" today ${DATA} ${DIR}/../
-find ${DIR}/../static/photos/ -type f -mtime -3 -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 800x800 -quality 75% {} \;
+find ${DIR}/../static/photos/ -type f -mtime -3 -iname "*.jpg" -size +150k -exec mogrify -verbose -format jpg -layers Dispose -resize 800x800 -quality 75% {} \;
 cd ${DIR}/..
 git add .
 git commit --no-gpg -am "Daily update"
